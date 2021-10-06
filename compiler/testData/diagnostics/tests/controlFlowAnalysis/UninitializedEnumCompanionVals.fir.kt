ea13a29a8db06b6cadf7a82aa76528f588c8d087
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-10-06 17:14:17 +0300

    Fix ClassCastException at FirUninitializedEnumChecker

---
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/UninitializedEnumCompanionVals.fir.kt
@@ -0,0 +1,17 @@
+// SKIP_TXT
+
+enum class A(z: Any) {
+    Y(<!UNINITIALIZED_VARIABLE!>x<!>);
+
+    companion object {
+        val x = A.Y.ordinal
+    }
+}
+
+enum class B(z: Any) {
+    Y(<!UNINITIALIZED_VARIABLE!>B.x<!>);
+
+    companion object {
+        val x = B.Y.ordinal
+    }
+}
