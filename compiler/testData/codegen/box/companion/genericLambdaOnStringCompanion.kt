b78c0865c529d2d156bb2bb0fe541598209ef898
Artem Kobzar <artem.kobzar@jetbrains.com>
2021-10-07 16:20:33 +0300

    test: rework Kt-47806 test for JVM.

--- compiler/testData/codegen/box/companion/genericLambdaOnStringCompanion.kt
+++ compiler/testData/codegen/box/companion/genericLambdaOnStringCompanion.kt
@@ -1,8 +1,10 @@
+// WITH_RUNTIME
+
 fun <T> T.f(E: (y: T) -> String): Boolean = E(this).isEmpty()
 
 fun fu1() = (String).f { v -> "" }
 
 fun box(): String {
-    assertEquals(fu1(), true)
+    if (!fu1()) return "Failed: Expect lambda to return empty string"
     return "OK"
 }
